442b9ab456463ce643c22b7fbad303ceca77b7b7
DavitDashyan <davit.dashyan@icloud.com>
2023-11-19 23:42:57 +0100

    works

--- libs/share-a-meal/features/src/lib/about/about.component.ts
+++ libs/share-a-meal/features/src/lib/about/about.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'avans-nx-workshop-about',
   templateUrl: './about.component.html',
   styles: [],
 })
-export class AboutComponent {}
+export class AboutComponent implements OnInit{
+
+  title = 'avans-nx-workshop-about';
+  imagePath?: string;
+  ngOnInit(): void {
+      this.imagePath = '/assets/erd.png';
+  }
+}
