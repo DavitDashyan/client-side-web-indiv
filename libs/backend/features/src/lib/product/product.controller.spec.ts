9f60b67218eadc424fc54d5762f46a38815b106c
DavitDashyan <davit.dashyan@icloud.com>
2023-12-10 22:34:25 +0100

    updated code 6

--- libs/backend/features/src/lib/product/product.controller.spec.ts
+++ libs/backend/features/src/lib/product/product.controller.spec.ts
@@ -1,6 +1,26 @@
+// import { Test, TestingModule } from '@nestjs/testing';
+// import { ProductController } from './product.controller';
+// import { ProductService } from './product.service';
+
+// describe('ProductController', () => {
+//   let controller: ProductController;
+
+//   beforeEach(async () => {
+//     const module: TestingModule = await Test.createTestingModule({
+//       controllers: [ProductController],
+//       providers: [ProductService],
+//     }).compile();
+
+//     controller = module.get<ProductController>(ProductController);
+//   });
+
+//   it('should be defined', () => {
+//     expect(controller).toBeDefined();
+//   });
+// });
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductController } from './product.controller';
-import { ProductService } from './product.service';
 
 describe('ProductController', () => {
   let controller: ProductController;
@@ -8,7 +28,6 @@ describe('ProductController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductController],
-      providers: [ProductService],
     }).compile();
 
     controller = module.get<ProductController>(ProductController);
